3754d960c825cda80e43e6ffd9ef904e3a551d2d
Minicapinon <bava080502hgtrtna6@cbtis217.edu.mx>
2024-10-28 08:17:26 -0600

    Update suma.cpp
    Added comments

--- suma.cpp
+++ suma.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
 using namespace std;
  // obed
-int main (){
+int main ()
+{
     int x, y, d;
-    cin>>x>>y>>d;
-    cout<<x+y+d;
+    cin >> x >> y >> d;
+    cout << x + y + d;
     
     return 0;
 }
-/*=======
+/*
+Esta parte la escribio Victor Hugo, pero Obed Sobre escribio este archivo
  int main()   {
     int a, b;
     cout<<"ingrese dos numeros para la suma"<<endl;
