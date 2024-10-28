64501c18b1e544469280facf7b602da1ae531396
Minicapinon <bava080502hgtrtna6@cbtis217.edu.mx>
2024-10-28 07:54:33 -0600

    Se anadio una funcion

---
+++ functon.cpp
@@ -0,0 +1,16 @@
+/*
+	Archvo creado por Minicapinon
+	Este archivo contiene una funcion para multiplicar dos valores
+*/
+
+#ifndef FUNCTON
+#define FUNCTON
+
+double multip(int vala, int valb)
+{
+	return vala * valb;
+}
+
+#endif //FUNCTON
+
+
